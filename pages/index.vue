8b8301efbe222680c582adabfc1745c8246ddbdd
Kazunari-h <kazunario456@gmail.com>
2020-06-12 16:27:22 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="columns is-multiline">
+      <div v-for="restaurant in restaurants" :key="restaurant.id" class="column is-4">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img :src="restaurant.thumbnail" />
+            </figure>
+          </div>
+          <div class="card-content">
+            <h1 class="title is-5">{{ restaurant.name }}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  components: {},
+  async asyncData({ app }) {
+    const response = await axios.get(
+      `https://www.master-7rqtwti-mq325chc5wubq.us-2.platformsh.site/Restaurant-Khs`
+    );
+
+    const restaurantsData = response.data.map(item => {
+      return {
+        id: item.id,
+        name: item.Name,
+        description: item.description,
+        thumbnail: item.photo.length
+          ? `https://www.master-7rqtwti-mq325chc5wubq.us-2.platformsh.site/${item.photo[0].url}`
+          : "http://placehold.jp/400x300.png?text=No Image"
+      };
+    });
+
+    return {
+      restaurants: restaurantsData
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
